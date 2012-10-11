1d0a31184edefd3f107343215d5263a5ac53cd56
Tamas Drahos <drahostamas@gmail.com>
2012-10-11 19:20:52 +0200

    menu current class

--- app/overrides/posts_in_header.rb
+++ app/overrides/posts_in_header.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "posts_in_header",
                      :insert_bottom => "#main-nav-bar",
-                     :text => "<li><%= link_to( t('posts'), posts_path) %></li>")
+                     :text => "<li class=\"<%= 'current' if request.path == posts_path %>\"><%= link_to( t('posts'), posts_path) %></li>")
